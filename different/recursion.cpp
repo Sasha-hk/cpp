9285d8a3ec9b121d6a833de3086b4cc0eb1131fb
Sasha-hk <sachapos50@gmail.com>
2022-01-28 10:00:17 +0200

    Create recursion.cpp

---
+++ different/recursion.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+
+using namespace std;
+
+int f(int n) {
+    if (n == 0) {
+        n = 1;
+    }
+    else {
+        n = f(n-1)*n;
+    }
+
+    return n;
+}
+
+int fLoop(int n) {
+    int factorial = 1;
+
+    for (int i = 2; i < n + 1; i++) {
+        factorial = factorial * i;
+    }
+
+    return factorial;
+}
+
+int main() {
+    int n;
+
+    cout << "Enter n: ";
+    cin >> n;
+
+    cout << "Recursion: " << f(n) << endl;
+    cout << "Loop:      " << fLoop(n) << endl;
+
+    return 0;
+}
